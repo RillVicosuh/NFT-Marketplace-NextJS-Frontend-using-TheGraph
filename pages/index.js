8fe429144aea49c6302d4dec3bd06928ce8cc1b7
RillVicosuh <rickvill00@gmail.com>
2023-05-28 00:11:38 -0700

    Added functionality to the sell page. Uploaded abi and contract info for the sepolia testnet

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
 //Won't need Cloud Function or frp folders with the cloud. Also for the graph, index.js and app.js will be different
 export default function Home() {
   return (
     <div className={styles.container}>
-      Hi!
+
     </div>
   )
 }
